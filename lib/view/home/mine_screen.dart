0d8f8d5c779f0036e4cb3778a19f4b45eb72b48b
chenjidong <374122600@qq.com>
2019-04-01 16:25:10 +0800

    添加设置界面，退出登录功能

--- lib/view/home/mine_screen.dart
+++ lib/view/home/mine_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_tool/view/account/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:flutter_my_tool/view/account/login_screen.dart';
 import '../../util/toast_util.dart';
+import '../mine/setting_screen.dart';
 
 ///个人中心
 class MineScreen extends StatefulWidget {
@@ -22,7 +23,9 @@ class _MineScreenState extends State<MineScreen> {
 
     bool isSignin = _sharedPreferences.getBool("is_signin");
     if (isSignin ?? false) {
-      ToastUtil.showToast('正在跳转设置界面');
+//      ToastUtil.showToast('正在跳转设置界面');
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => SettingScreen()));
     } else {
       ToastUtil.showToast('请登录后重试');
       Navigator.of(context)
---
+++ lib/view/mine/setting_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../util/toast_util.dart';
+
+class SettingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SettingState();
+  }
+}
+
+class _SettingState extends State<SettingScreen> {
+  /// 退出登录
+  void _logout() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool('is_signin', false);
+    ToastUtil.showToast('退出成功！');
+    Navigator.of(context).pop(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(builder: (context) {
+          return IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.of(context).pop(this);
+              });
+        }),
+      ),
+      body: SafeArea(
+          child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: GestureDetector(
+              onTap: () {
+                ToastUtil.showToast('已经是最新版本');
+              },
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.settings,
+                    color: Colors.blue,
+                    size: 18,
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Text(
+                          '版本号：',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(color: Colors.blue, fontSize: 16),
+                        ),
+                      )),
+                  Text(
+                    '1.0.0',
+                    style: TextStyle(color: Colors.blue, fontSize: 14),
+                  ),
+                  Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.blue,
+                    size: 12,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: RaisedButton(
+                      onPressed: () {
+                        _logout();
+                      },
+                      color: Colors.red,
+                      child: Text(
+                        '退出登录',
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    ))
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
